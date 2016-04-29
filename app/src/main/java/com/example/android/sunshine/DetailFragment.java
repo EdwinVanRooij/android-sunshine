66a7959a4acbbb5988d0c64a58c656344407337f
Edwin <edwinvrooij@gmail.com>
2016-04-29 20:00:19 +0200

    included last lessons of the tutorial

---
+++ app/src/main/java/com/example/android/sunshine/DetailFragment.java
@@ -0,0 +1,86 @@
+package com.example.android.sunshine;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DetailFragment extends Fragment {
+
+    private static final String TAG = "DetailFragment";
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private String mForecastStr;
+
+    public DetailFragment() {
+        // Required empty public constructor
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        Intent i = getActivity().getIntent();
+        TextView text = (TextView) root.findViewById(R.id.detail_text);
+        mForecastStr = i.getExtras().getString(Intent.EXTRA_TEXT);
+        text.setText(mForecastStr);
+
+
+        return root;
+    }
+
+    private Intent createShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastStr + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.detailfragment, menu);
+
+        MenuItem item = menu.findItem(R.id.action_share);
+
+        ShareActionProvider mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareIntent());
+        } else {
+            Log.d(TAG, "onCreateOptionsMenu: share action provider is null");
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_share) {
+            startActivity(createShareIntent());
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
